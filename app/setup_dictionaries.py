9a44d0644fc9e0f8a9f57a99d3c4298704753e3c
balaurian <46764335+balaurian@users.noreply.github.com>
2024-05-25 00:29:41 +0300

    Add files via upload
    - removed unused nltk import module
    - removed parse_dates=[2] parameter from pd.read_csv, it was generating a UserWarning

--- app/setup_dictionaries.py
+++ app/setup_dictionaries.py
@@ -1,4 +1,3 @@
-import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
@@ -14,12 +13,13 @@ with open('app/data/fx-bear-terms.csv', 'r') as bears:
 with open('app/data/fx-bull-terms.csv', 'r') as bulls:
     bull_words = bulls.read()
 
-#with open('app/data/fx-pairs-slang.csv', 'r') as pairs:
-#    pair_vocabulary = pairs.read()
-
 pair_list = pd.read_csv('app/data/fx-pairs-collection.csv', header = 0, sep = ',', on_bad_lines = "skip", encoding = 'utf-8')
 
-pair_vocabulary = pd.read_csv('app/data/fx-pairs-slang.csv', header = 0, sep = ',', on_bad_lines = "skip", encoding = 'utf-8', parse_dates = [2])
+pair_vocabulary = pd.read_csv('app/data/fx-pairs-slang.csv',
+                              header = 0,
+                              sep = ',',
+                              on_bad_lines = "skip",
+                              encoding = 'utf-8')
 
 def pair_to_dictionary(pair_vocabulary):
     pair_vocab_dic = defaultdict(dict)
