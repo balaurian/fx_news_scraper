fc63bb769895ba71f222cea09341d71b9aa83906
balaurian <46764335+balaurian@users.noreply.github.com>
2024-05-25 00:22:08 +0300

    Add files via upload
    - updated div navigation and classes
    - save csv after parsing each page
    - added tqdm progress

--- app/scraper.py
+++ app/scraper.py
@@ -1,72 +1,46 @@
-from urllib.request import Request, urlopen
-import datetime
-
 from bs4 import BeautifulSoup as bs
 import pandas as pd
+from urllib.request import Request, urlopen
+from tqdm import tqdm
 
 class fx_news_scraper():
     def __init__(self):
-        # 
-        # The scraped csv file is written on disk after the whole scraping is done. 
-        # This is a problem when the connection drops and all scraping is lost 
-        # Therefore you need to set how many pages you would need to scrap. 
-        # Keep this number low while testing/debugging
-        #
-        self.number_of_pages = 3
+        self.number_of_pages = 29
         self.news_list = []
         self.target_url = 'https://www.investing.com/news/forex-news/'
-        self.output_csv = 'app/data/fx-news-archive.csv'
-
+        self.output_csv = 'app/data/fx-news-archive_test.csv'
+        self.news_dataset = pd.DataFrame()
 
     def scraper(self):
-        for page_no in range(1, self.number_of_pages) :
+        for page_no in tqdm(range(1, self.number_of_pages+1)):
             url = self.target_url + str(page_no)
-
             req = Request(url , headers={'User-Agent': 'Mozilla/5.0'})
             
             webpage = urlopen(req).read()
-
+            
             soup = bs(webpage, "html.parser")
             
-            div_navigation = soup.find('div', id = 'paginationWrap')
+            news_container_div = soup.find('div', class_= 'news-analysis-v2_articles-container__3fFL8 mdMax:px-3 mb-12')
 
-            navigation = div_navigation.find('div', class_ = 'sideDiv inlineblock text_align_lang_base_2').text
-            
-            if len(navigation) != 1:
-                
-                div = soup.find('div', class_= 'largeTitle')
-                
-                now = datetime.datetime.now()
-                date = datetime.datetime
-
-                for news_div in div.find_all('div', class_= 'textDiv'):
-                    news_title_a = news_div.find('a', class_ = 'title')
+            try:
+                for news_div in news_container_div.find_all('div', class_= 'news-analysis-v2_content__z0iLP w-full text-xs sm:flex-1'):
+                    news_title_a = news_div.find('a', class_ = 'text-inv-blue-500 hover:text-inv-blue-500 hover:underline focus:text-inv-blue-500 focus:underline whitespace-normal text-sm font-bold leading-5 !text-[#181C21] sm:text-base sm:leading-6 lg:text-lg lg:leading-7')
                     news_body    = news_div.find('p')
-                    news_date    = news_div.find('span', class_ = 'date')
+                    news_date    = news_div.find('time', class_ = 'mx-1 shrink-0 text-xs leading-4').get('datetime')
+                    
                     news_href    = news_title_a.get('href')
-
-                    if news_date is not None:
-                        news_date = news_date.text
-                        
-                        if 'hours' in news_date:
-                            news_date = now.strftime("\xa0-\xa0%b %d, %Y")
-                        if 'hour' in news_date:
-                            news_date = now.strftime("\xa0-\xa0%b %d, %Y")
-                        if 'minutes' in news_date:
-                            news_date = now.strftime("\xa0-\xa0%b %d, %Y")
-
-                        if '-' in news_date:
-                            date_news_date = date.strptime(news_date, "\xa0-\xa0%b %d, %Y")
-                            
-                            self.news_list.append([page_no, date_news_date, news_title_a.text , news_body.text ])
-                            
-                        news_dataset = pd.DataFrame(self.news_list, columns = ['page', 'date','title','body'] )
                     
-                page_no += 1
-                    
-            else:
-                continue
+                    self.news_list.append([page_no, news_date, news_title_a.text , news_body.text, news_href])
 
-        news_dataset.to_csv(self.output_csv)
+                    
+            except ValueError as vEr:
+                print(vEr)
+
+            
+            self.news_dataset = pd.DataFrame(self.news_list, columns = ['page', 'date','title','body', 'url'] )
+            self.news_dataset.to_csv(self.output_csv)
+            
+            page_no += 1
+
         print ('scraping completed')
-        print ('output --> app/data/fx-news-archive.csv')
+        print (f'output --> {self.output_csv}')
